a02775dca4dd05ae6a58ea69ba8339fe63b25b63
Daniela García <DanyGa02@hotmail.com>
2020-12-17 09:43:43 -0600

    :)

--- ProyectoFinal/ProyectoFinal/EstructurasdeDatos/TiposdeListas/ListaSimple.Designer.cs
+++ ProyectoFinal/ProyectoFinal/EstructurasdeDatos/TiposdeListas/ListaSimple.Designer.cs
@@ -28,12 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListaSimple));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblDa = new System.Windows.Forms.Label();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.lblMostrar = new System.Windows.Forms.Label();
+            this.lblBo = new System.Windows.Forms.Label();
+            this.txtNodo = new System.Windows.Forms.TextBox();
+            this.txtDatoBorrar = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminarLista = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnGuardarArchivo = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.algoritmosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estructurasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Snow;
+            this.lblTitulo.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblTitulo.Location = new System.Drawing.Point(198, 39);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(207, 37);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Lista Simple";
+            // 
+            // lblDa
+            // 
+            this.lblDa.AutoSize = true;
+            this.lblDa.BackColor = System.Drawing.Color.Snow;
+            this.lblDa.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDa.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblDa.Location = new System.Drawing.Point(24, 104);
+            this.lblDa.Name = "lblDa";
+            this.lblDa.Size = new System.Drawing.Size(57, 22);
+            this.lblDa.TabIndex = 1;
+            this.lblDa.Text = "Dato:";
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.BackColor = System.Drawing.Color.Snow;
+            this.lblLista.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLista.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblLista.Location = new System.Drawing.Point(218, 104);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(53, 22);
+            this.lblLista.TabIndex = 2;
+            this.lblLista.Text = "Lista";
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.AutoSize = true;
+            this.lblMostrar.BackColor = System.Drawing.Color.Snow;
+            this.lblMostrar.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrar.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblMostrar.Location = new System.Drawing.Point(218, 142);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(45, 22);
+            this.lblMostrar.TabIndex = 3;
+            this.lblMostrar.Text = "-------";
+            // 
+            // lblBo
+            // 
+            this.lblBo.AutoSize = true;
+            this.lblBo.BackColor = System.Drawing.Color.Snow;
+            this.lblBo.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBo.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblBo.Location = new System.Drawing.Point(24, 269);
+            this.lblBo.Name = "lblBo";
+            this.lblBo.Size = new System.Drawing.Size(140, 22);
+            this.lblBo.TabIndex = 4;
+            this.lblBo.Text = "Dato a borrar:";
+            // 
+            // txtNodo
+            // 
+            this.txtNodo.Location = new System.Drawing.Point(87, 104);
+            this.txtNodo.Name = "txtNodo";
+            this.txtNodo.Size = new System.Drawing.Size(100, 22);
+            this.txtNodo.TabIndex = 5;
+            // 
+            // txtDatoBorrar
+            // 
+            this.txtDatoBorrar.Location = new System.Drawing.Point(170, 269);
+            this.txtDatoBorrar.Name = "txtDatoBorrar";
+            this.txtDatoBorrar.Size = new System.Drawing.Size(100, 22);
+            this.txtDatoBorrar.TabIndex = 6;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Snow;
+            this.btnGuardar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnGuardar.Location = new System.Drawing.Point(18, 142);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(175, 33);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar y mostrar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEliminarLista
+            // 
+            this.btnEliminarLista.BackColor = System.Drawing.Color.Snow;
+            this.btnEliminarLista.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarLista.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnEliminarLista.Location = new System.Drawing.Point(33, 184);
+            this.btnEliminarLista.Name = "btnEliminarLista";
+            this.btnEliminarLista.Size = new System.Drawing.Size(131, 33);
+            this.btnEliminarLista.TabIndex = 8;
+            this.btnEliminarLista.Text = "Eliminar lista";
+            this.btnEliminarLista.UseVisualStyleBackColor = false;
+            this.btnEliminarLista.Click += new System.EventHandler(this.btnEliminarLista_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.Snow;
+            this.btnBorrar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnBorrar.Location = new System.Drawing.Point(276, 265);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(85, 32);
+            this.btnBorrar.TabIndex = 9;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.Snow;
+            this.btnMostrar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnMostrar.Location = new System.Drawing.Point(452, 104);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(98, 45);
+            this.btnMostrar.TabIndex = 10;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnContar
+            // 
+            this.btnContar.BackColor = System.Drawing.Color.Snow;
+            this.btnContar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnContar.Location = new System.Drawing.Point(452, 156);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(98, 45);
+            this.btnContar.TabIndex = 11;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = false;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.Color.Snow;
+            this.btnCargar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnCargar.Location = new System.Drawing.Point(452, 207);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(98, 45);
+            this.btnCargar.TabIndex = 12;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnGuardarArchivo
+            // 
+            this.btnGuardarArchivo.BackColor = System.Drawing.Color.Snow;
+            this.btnGuardarArchivo.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarArchivo.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnGuardarArchivo.Location = new System.Drawing.Point(452, 259);
+            this.btnGuardarArchivo.Name = "btnGuardarArchivo";
+            this.btnGuardarArchivo.Size = new System.Drawing.Size(98, 45);
+            this.btnGuardarArchivo.TabIndex = 13;
+            this.btnGuardarArchivo.Text = "Guardar";
+            this.btnGuardarArchivo.UseVisualStyleBackColor = false;
+            this.btnGuardarArchivo.Click += new System.EventHandler(this.btnGuardarArchivo_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.algoritmosToolStripMenuItem,
+            this.estructurasToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(576, 28);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // algoritmosToolStripMenuItem
+            // 
+            this.algoritmosToolStripMenuItem.Name = "algoritmosToolStripMenuItem";
+            this.algoritmosToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.algoritmosToolStripMenuItem.Text = "Algoritmos";
+            this.algoritmosToolStripMenuItem.Click += new System.EventHandler(this.algoritmosToolStripMenuItem_Click);
+            // 
+            // estructurasToolStripMenuItem
+            // 
+            this.estructurasToolStripMenuItem.Name = "estructurasToolStripMenuItem";
+            this.estructurasToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.estructurasToolStripMenuItem.Text = "Estructuras";
+            this.estructurasToolStripMenuItem.Click += new System.EventHandler(this.estructurasToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // ListaSimple
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ListaSimple";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(576, 349);
+            this.Controls.Add(this.btnGuardarArchivo);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEliminarLista);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtDatoBorrar);
+            this.Controls.Add(this.txtNodo);
+            this.Controls.Add(this.lblBo);
+            this.Controls.Add(this.lblMostrar);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.lblDa);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ListaSimple";
+            this.Text = "Lista Simple";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblDa;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.Label lblMostrar;
+        private System.Windows.Forms.Label lblBo;
+        private System.Windows.Forms.TextBox txtNodo;
+        private System.Windows.Forms.TextBox txtDatoBorrar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminarLista;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnGuardarArchivo;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem algoritmosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estructurasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
     }
 }
--- ProyectoFinal/ProyectoFinal/EstructurasdeDatos/TiposdeListas/ListaSimple.cs
+++ ProyectoFinal/ProyectoFinal/EstructurasdeDatos/TiposdeListas/ListaSimple.cs
@@ -19,5 +19,114 @@ namespace ProyectoFinal.EstructurasdeDatos.TiposdeListas
         {
             InitializeComponent();
         }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!MiLista.BuscarDato(int.Parse(txtNodo.Text)))
+                {
+                    n = new Nodo();
+                    n.Dato = int.Parse(txtNodo.Text);
+                    MiLista.Agregar(n);
+                    lblLista.Text = MiLista.ToString();
+                    txtNodo.Clear();
+                    return;
+                }
+                MessageBox.Show("El dato ya existe en la lista");
+                txtNodo.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Introduzca un numero valido");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            int dato = int.Parse(txtDatoBorrar.Text);
+            if (!MiLista.BuscarDato(dato))
+            {
+                MessageBox.Show("No se encontro el dato");
+                lblLista.Text = MiLista.ToString();
+                return;
+            }
+            MiLista.Borrar(dato);
+            lblLista.Text = MiLista.ToString();
+            txtDatoBorrar.Clear();
+        }
+
+        private void btnEliminarLista_Click(object sender, EventArgs e)
+        {
+            MiLista.Head = null;
+            lblLista.Text = MiLista.ToString();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lblMostrar.Text = MiLista.ToString();
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Numero de nodos en la lista: " + MiLista.ContarNodos());
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Seleccionar = new OpenFileDialog();
+            if (Seleccionar.ShowDialog() == DialogResult.OK)
+            {
+                MiLista.Head = null;
+                int contador = 0;
+                string ruta = Seleccionar.FileName;
+                string linea = File.ReadAllText(ruta);
+                string[] Lista = linea.Split(',');
+                foreach (string i in Lista)
+                {
+                    n = new Nodo();
+                    n.Dato = int.Parse(Lista[contador]);
+                    MiLista.Agregar(n);
+                    lblLista.Text = MiLista.ToString();
+                    contador++;
+                }
+
+            }
+        }
+
+        private void btnGuardarArchivo_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Dialogo = new FolderBrowserDialog();
+            if (Dialogo.ShowDialog() == DialogResult.OK)
+            {
+                string dato = lblLista.Text;
+                string ruta = Dialogo.SelectedPath + "\\Lista.txt";
+                using (var writer = new StreamWriter(ruta))
+                {
+                    writer.Close();
+                }
+                File.WriteAllText(ruta, dato);
+                MessageBox.Show("Datos guardados");
+            }
+        }
+
+        private void algoritmosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MenuAlgoritmos noransaek = new MenuAlgoritmos();
+            noransaek.Show();
+            this.Close();
+        }
+
+        private void estructurasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MenuEstructuras joohwangsaek = new MenuEstructuras();
+            joohwangsaek.Show();
+            this.Close();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
