58896e9df4c8a1e20cc3b995cb7df88372c3b3e8
Daniela García <DanyGa02@hotmail.com>
2020-12-17 22:36:45 -0600

    :)

--- ProyectoFinal/ProyectoFinal/Menu.Designer.cs
+++ ProyectoFinal/ProyectoFinal/Menu.Designer.cs
@@ -29,35 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
-            this.lBi = new System.Windows.Forms.Label();
-            this.lPr = new System.Windows.Forms.Label();
+            this.lblBi = new System.Windows.Forms.Label();
+            this.lblPr = new System.Windows.Forms.Label();
             this.btnAlgoritmo = new System.Windows.Forms.Button();
             this.btnEstructura = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lBi
+            // lblBi
             // 
-            this.lBi.AutoSize = true;
-            this.lBi.BackColor = System.Drawing.Color.Transparent;
-            this.lBi.Font = new System.Drawing.Font("Elephant", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lBi.ForeColor = System.Drawing.Color.Snow;
-            this.lBi.Location = new System.Drawing.Point(171, 23);
-            this.lBi.Name = "lBi";
-            this.lBi.Size = new System.Drawing.Size(285, 55);
-            this.lBi.TabIndex = 0;
-            this.lBi.Text = "Bienvenid@";
+            this.lblBi.AutoSize = true;
+            this.lblBi.BackColor = System.Drawing.Color.Transparent;
+            this.lblBi.Font = new System.Drawing.Font("Elephant", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBi.ForeColor = System.Drawing.Color.Snow;
+            this.lblBi.Location = new System.Drawing.Point(171, 23);
+            this.lblBi.Name = "lblBi";
+            this.lblBi.Size = new System.Drawing.Size(285, 55);
+            this.lblBi.TabIndex = 0;
+            this.lblBi.Text = "Bienvenid@";
             // 
-            // lPr
+            // lblPr
             // 
-            this.lPr.AutoSize = true;
-            this.lPr.BackColor = System.Drawing.Color.Transparent;
-            this.lPr.Font = new System.Drawing.Font("Elephant", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lPr.ForeColor = System.Drawing.Color.Snow;
-            this.lPr.Location = new System.Drawing.Point(131, 78);
-            this.lPr.Name = "lPr";
-            this.lPr.Size = new System.Drawing.Size(364, 51);
-            this.lPr.TabIndex = 1;
-            this.lPr.Text = "Que desea crear?";
+            this.lblPr.AutoSize = true;
+            this.lblPr.BackColor = System.Drawing.Color.Transparent;
+            this.lblPr.Font = new System.Drawing.Font("Elephant", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPr.ForeColor = System.Drawing.Color.Snow;
+            this.lblPr.Location = new System.Drawing.Point(131, 78);
+            this.lblPr.Name = "lblPr";
+            this.lblPr.Size = new System.Drawing.Size(364, 51);
+            this.lblPr.TabIndex = 1;
+            this.lblPr.Text = "Que desea crear?";
             // 
             // btnAlgoritmo
             // 
@@ -94,8 +94,8 @@
             this.ClientSize = new System.Drawing.Size(624, 272);
             this.Controls.Add(this.btnEstructura);
             this.Controls.Add(this.btnAlgoritmo);
-            this.Controls.Add(this.lPr);
-            this.Controls.Add(this.lBi);
+            this.Controls.Add(this.lblPr);
+            this.Controls.Add(this.lblBi);
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -107,8 +107,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lBi;
-        private System.Windows.Forms.Label lPr;
+        private System.Windows.Forms.Label lblBi;
+        private System.Windows.Forms.Label lblPr;
         private System.Windows.Forms.Button btnAlgoritmo;
         private System.Windows.Forms.Button btnEstructura;
     }
