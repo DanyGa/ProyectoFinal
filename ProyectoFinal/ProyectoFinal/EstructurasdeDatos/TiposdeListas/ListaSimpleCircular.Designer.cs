4799d356ea4f3c25f0828d0361de559e71a0c0af
Daniela García <DanyGa02@hotmail.com>
2020-12-17 21:50:37 -0600

    :)

--- ProyectoFinal/ProyectoFinal/EstructurasdeDatos/TiposdeListas/ListaSimpleCircular.Designer.cs
+++ ProyectoFinal/ProyectoFinal/EstructurasdeDatos/TiposdeListas/ListaSimpleCircular.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListaSimpleCircular));
             this.lblTitulo = new System.Windows.Forms.Label();
             this.lblLista = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -48,23 +49,30 @@
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(228, 58);
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Elephant", 22.2F);
+            this.lblTitulo.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lblTitulo.Location = new System.Drawing.Point(145, 44);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(136, 17);
+            this.lblTitulo.Size = new System.Drawing.Size(433, 49);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Lista Simple Circular";
             // 
             // lblLista
             // 
             this.lblLista.AutoSize = true;
-            this.lblLista.Location = new System.Drawing.Point(174, 152);
+            this.lblLista.BackColor = System.Drawing.Color.Transparent;
+            this.lblLista.Font = new System.Drawing.Font("Britannic Bold", 16.2F);
+            this.lblLista.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lblLista.Location = new System.Drawing.Point(212, 158);
             this.lblLista.Name = "lblLista";
-            this.lblLista.Size = new System.Drawing.Size(42, 17);
+            this.lblLista.Size = new System.Drawing.Size(81, 31);
             this.lblLista.TabIndex = 1;
             this.lblLista.Text = "Lista:";
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Snow;
             this.menuStrip1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -73,104 +81,125 @@
             this.salirToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(753, 28);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // algoritmosToolStripMenuItem
             // 
             this.algoritmosToolStripMenuItem.Name = "algoritmosToolStripMenuItem";
-            this.algoritmosToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.algoritmosToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
             this.algoritmosToolStripMenuItem.Text = "Algoritmos";
             this.algoritmosToolStripMenuItem.Click += new System.EventHandler(this.algoritmosToolStripMenuItem_Click);
             // 
             // estructurasToolStripMenuItem
             // 
             this.estructurasToolStripMenuItem.Name = "estructurasToolStripMenuItem";
-            this.estructurasToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.estructurasToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
             this.estructurasToolStripMenuItem.Text = "Estructuras";
             this.estructurasToolStripMenuItem.Click += new System.EventHandler(this.estructurasToolStripMenuItem_Click);
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
             // txtNodo
             // 
-            this.txtNodo.Location = new System.Drawing.Point(244, 268);
+            this.txtNodo.Location = new System.Drawing.Point(311, 284);
             this.txtNodo.Name = "txtNodo";
-            this.txtNodo.Size = new System.Drawing.Size(100, 22);
+            this.txtNodo.Size = new System.Drawing.Size(143, 22);
             this.txtNodo.TabIndex = 3;
             // 
             // btnInsertar
             // 
-            this.btnInsertar.Location = new System.Drawing.Point(36, 126);
+            this.btnInsertar.BackColor = System.Drawing.Color.Snow;
+            this.btnInsertar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold);
+            this.btnInsertar.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnInsertar.Location = new System.Drawing.Point(74, 123);
             this.btnInsertar.Name = "btnInsertar";
-            this.btnInsertar.Size = new System.Drawing.Size(75, 43);
+            this.btnInsertar.Size = new System.Drawing.Size(110, 58);
             this.btnInsertar.TabIndex = 4;
             this.btnInsertar.Text = "Insertar";
-            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.UseVisualStyleBackColor = false;
             this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
             // 
             // btnBorrar
             // 
-            this.btnBorrar.Location = new System.Drawing.Point(36, 202);
+            this.btnBorrar.BackColor = System.Drawing.Color.Snow;
+            this.btnBorrar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold);
+            this.btnBorrar.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnBorrar.Location = new System.Drawing.Point(74, 214);
             this.btnBorrar.Name = "btnBorrar";
-            this.btnBorrar.Size = new System.Drawing.Size(75, 43);
+            this.btnBorrar.Size = new System.Drawing.Size(110, 58);
             this.btnBorrar.TabIndex = 5;
             this.btnBorrar.Text = "Borrar";
-            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.UseVisualStyleBackColor = false;
             this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
             // 
             // btnEliminarLista
             // 
-            this.btnEliminarLista.Location = new System.Drawing.Point(36, 280);
+            this.btnEliminarLista.BackColor = System.Drawing.Color.Snow;
+            this.btnEliminarLista.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold);
+            this.btnEliminarLista.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnEliminarLista.Location = new System.Drawing.Point(74, 305);
             this.btnEliminarLista.Name = "btnEliminarLista";
-            this.btnEliminarLista.Size = new System.Drawing.Size(75, 43);
+            this.btnEliminarLista.Size = new System.Drawing.Size(110, 58);
             this.btnEliminarLista.TabIndex = 6;
             this.btnEliminarLista.Text = "Eliminar lista";
-            this.btnEliminarLista.UseVisualStyleBackColor = true;
+            this.btnEliminarLista.UseVisualStyleBackColor = false;
             this.btnEliminarLista.Click += new System.EventHandler(this.btnEliminarLista_Click);
             // 
             // btnContar
             // 
-            this.btnContar.Location = new System.Drawing.Point(429, 126);
+            this.btnContar.BackColor = System.Drawing.Color.Snow;
+            this.btnContar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold);
+            this.btnContar.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnContar.Location = new System.Drawing.Point(524, 123);
             this.btnContar.Name = "btnContar";
-            this.btnContar.Size = new System.Drawing.Size(75, 43);
+            this.btnContar.Size = new System.Drawing.Size(110, 58);
             this.btnContar.TabIndex = 7;
             this.btnContar.Text = "Contar";
-            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.UseVisualStyleBackColor = false;
             this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
             // 
             // btnGuardarArchivo
             // 
-            this.btnGuardarArchivo.Location = new System.Drawing.Point(429, 202);
+            this.btnGuardarArchivo.BackColor = System.Drawing.Color.Snow;
+            this.btnGuardarArchivo.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold);
+            this.btnGuardarArchivo.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnGuardarArchivo.Location = new System.Drawing.Point(524, 214);
             this.btnGuardarArchivo.Name = "btnGuardarArchivo";
-            this.btnGuardarArchivo.Size = new System.Drawing.Size(75, 43);
+            this.btnGuardarArchivo.Size = new System.Drawing.Size(110, 58);
             this.btnGuardarArchivo.TabIndex = 8;
             this.btnGuardarArchivo.Text = "Guardar Archivo";
-            this.btnGuardarArchivo.UseVisualStyleBackColor = true;
+            this.btnGuardarArchivo.UseVisualStyleBackColor = false;
             this.btnGuardarArchivo.Click += new System.EventHandler(this.btnGuardarArchivo_Click);
             // 
             // btnCargar
             // 
-            this.btnCargar.Location = new System.Drawing.Point(429, 280);
+            this.btnCargar.BackColor = System.Drawing.Color.Snow;
+            this.btnCargar.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCargar.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnCargar.Location = new System.Drawing.Point(524, 305);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(75, 43);
+            this.btnCargar.Size = new System.Drawing.Size(110, 58);
             this.btnCargar.TabIndex = 9;
             this.btnCargar.Text = "Cargar";
-            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.UseVisualStyleBackColor = false;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(174, 271);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Britannic Bold", 16.2F);
+            this.label3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Location = new System.Drawing.Point(212, 276);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.Size = new System.Drawing.Size(79, 31);
             this.label3.TabIndex = 10;
             this.label3.Text = "Dato:";
             // 
@@ -178,7 +207,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(753, 427);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.btnGuardarArchivo);
